cb38a1aa90eb61dd323eb6793a7035cc6036ab39
meef1k <m4t3ush3k@gmail.com>
2022-03-26 15:53:46 +0100

    Done

---
+++ Fraction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace fraction
+{
+    class Fraction : IComparable<Fraction>
+    {
+        public readonly int numerator;
+        public readonly int denominator;
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Wrong denominator!");   
+            }
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+
+        public int CompareTo(Fraction f2)
+        {
+            int numerator1 = numerator * f2.denominator;
+            int numerator2 = f2.numerator * denominator;
+            if(numerator1 > numerator2)
+            {
+                return 1;
+            }
+            else if(numerator1 < numerator2)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            return new Fraction(f1.numerator * f2.denominator + f2.numerator * f1.denominator, f1.denominator * f2.denominator);
+        }
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            return new Fraction(f1.numerator * f2.denominator - f2.numerator * f1.denominator, f1.denominator * f2.denominator);
+        }
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            return new Fraction(f1.numerator * f2.numerator, f1.denominator * f2.denominator);
+        }
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            if (f1.numerator == 0 || f2.numerator == 0)
+            {
+                throw new ArgumentException("Wrong denominator!");
+            }
+            return new Fraction(f1.numerator * f2.denominator, f1.denominator * f2.numerator);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace fraction
 {
@@ -6,7 +7,22 @@ namespace fraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fraction f1 = new Fraction(1, 3);
+            Fraction f2 = new Fraction(1, 2);
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            Console.WriteLine(f1 + f2);
+            Console.WriteLine(f1 - f2);
+            Console.WriteLine(f1 * f2);
+            Console.WriteLine(f1 / f2);
+
+            List<Fraction> list = new List<Fraction>();
+            list.Add(f1);
+            list.Add(f2);
+            list.Add(new Fraction(4, 5));
+            list.Add(new Fraction(7, 3));
+            list.Sort();
+            list.ForEach(Console.WriteLine);
         }
     }
 }
